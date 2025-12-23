6893f059550a25ecc079f8974484a8bc529c5cf9
Shaswagupta <shaswagupta@gmail.com>
2025-12-23 21:47:52 +0530

    calculater is ready

---
+++ main.py
@@ -0,0 +1,38 @@
+def calculation ( number,number2):
+    operation = input("""choose the operation which you want to perform on numbers:
+                    '+' for adding ,
+                    '-' for subtracting ,
+                    '*' for multiplication ,
+                    '/' for divide 
+""")
+    if operation == "+":
+        return number + number2
+    elif operation == "-":
+        return number - number2
+    elif operation == "*":
+        return number * number2
+    elif operation == "%":
+        return number % number2
+    else :
+        return 0 
+
+num1 = int(input("enter a  1st number : "))
+num2 = int(input("enter a  2nd number : "))
+result = calculation(num1,num2)
+should =True
+
+while should:
+    
+    play_more = input("want perform operation on more number \n     'yes' or 'no' \n        ") 
+    if play_more =="yes":
+        # while should :
+            print(f"so the result is {result}")
+            num3 = int(input("enter a number :"))
+            result = calculation(result,num3)
+            print(f"so the result is {result}")
+    else :
+        should = False
+        print(f"so the result is {result}")        
+
+
+  
